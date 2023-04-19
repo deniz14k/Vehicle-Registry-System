8f115e1abc43108d0d90bcded7783b2190f165c3
deniz14k <113996475+deniz14k@users.noreply.github.com>
2023-04-19 18:35:43 +0300

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -24,16 +24,33 @@ public class Exercise2 {
                 switch (choice) {
 
                     case 1:
-                                registry.addVehicle();
+                        System.out.println("Enter the VIN : ");
+                        int vin=read.nextInt();
+                        read.nextLine();
+                        System.out.println("Enter the License Plate  : ");
+                        String LicensePlate=read.nextLine();
+
+                        System.out.println("Enter the Make : ");
+                        String make=read.nextLine();
+                        System.out.println("Enter the Model : ");
+                        String model=read.nextLine();
+                        System.out.println("Enter the year : ");
+                        int year= read.nextInt();
+                        read.nextLine();
+                                registry.addVehicle(vin,LicensePlate,make,model,year,registry.getRegistry());
                         break;
 
 
                     case 2:
-                                registry.removeVehicle();
+                                System.out.println("Enter the VIN to delete : ");
+                                int vinToDelete=read.nextInt();
+                                registry.removeVehicle(vinToDelete, registry.getRegistry());
                         break;
 
                     case 3:
-                                registry.checkVehicle();
+                                System.out.println("Enter the VIN of the car to check if it is in the registry : ");
+                                int vinToSearch=read.nextInt();
+                                registry.checkVehicle(vinToSearch,registry.getRegistry());
 
                         break;
 
