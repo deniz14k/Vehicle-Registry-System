dff80598d51f5af989988ac46fbcb74a7391de9d
deniz14k <113996475+deniz14k@users.noreply.github.com>
2023-04-19 18:37:31 +0300

    Update TestExercise2.java

--- TestExercise2.java
+++ TestExercise2.java
@@ -1,13 +1,11 @@
 package isp.lab6.exercise2;
 import org.junit.Test;
 import java.util.HashSet;
-import java.util.Scanner;
 import static org.junit.Assert.assertEquals;
 
 
 public class TestExercise2 {
 
-    //CarInfo carinfo;
     Registry vehicles=new Registry();
     HashSet<CarInfo> registry=new HashSet<CarInfo>();
 
@@ -15,9 +13,8 @@ public class TestExercise2 {
 
     public void addVehicleTestForExistingVIN(){
 
-        //CarInfo carinfo=new CarInfo(1,"MS21DNZ","BMW","320d",2013);
-    vehicles.addVehicleWithParameters(1,"MS21DNZ","BMW","320d",2013,registry);
-    vehicles.addVehicleWithParameters(1,"CJ33LLL","FORD","TRANSIT",2016,registry);
+    vehicles.addVehicle(1,"MS21DNZ","BMW","320d",2013,registry);
+    vehicles.addVehicle(1,"CJ33LLL","FORD","TRANSIT",2016,registry);
 
     int expected=1;             // nu ar trebuii sa il bage pe al doilea ca are acelasi VIN
     int actual=registry.size();
@@ -29,10 +26,9 @@ public class TestExercise2 {
 
     public void RemoveVehicleTest(){
 
-        //CarInfo carinfo=new CarInfo(1,"MS21DNZ","BMW","320d",2013);
-        vehicles.addVehicleWithParameters(1,"MS21DNZ","BMW","320d",2013,registry);
-        vehicles.addVehicleWithParameters(2,"CJ33LLL","FORD","TRANSIT",2016,registry);
-        vehicles.removeVehicleWithParameters(2,registry);
+        vehicles.addVehicle(1,"MS21DNZ","BMW","320d",2013,registry);
+        vehicles.addVehicle(2,"CJ33LLL","FORD","TRANSIT",2016,registry);
+        vehicles.removeVehicle(2,registry);
         int expected=1;             // ar trebuii sa fie 1 pt ca am sters al doilea vehicul
         int actual=registry.size();
         assertEquals(expected,actual);
@@ -42,12 +38,11 @@ public class TestExercise2 {
     @Test
 
     public void CheckVehicleTest(){
-        boolean found=false;
-        //CarInfo carinfo=new CarInfo(1,"MS21DNZ","BMW","320d",2013);
-        vehicles.addVehicleWithParameters(1,"MS21DNZ","BMW","320d",2013,registry);
-        vehicles.addVehicleWithParameters(2,"CJ33LLL","FORD","TRANSIT",2016,registry);
+        
+        vehicles.addVehicle(1,"MS21DNZ","BMW","320d",2013,registry);
+        vehicles.addVehicle(2,"CJ33LLL","FORD","TRANSIT",2016,registry);
         boolean expected=true;
-        boolean actual=vehicles.checkVehicleWithParameters(1,registry);
+        boolean actual=vehicles.checkVehicle(1,registry);
         assertEquals(expected,actual);
 
     }
